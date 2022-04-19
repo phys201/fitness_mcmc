b458261049aabb868f3ca0c646d9783da0009412
Lidiya Ahmed <lidiyayosefah78@gmail.com>
2022-04-19 17:16:19 -0400

    created tests in tests folder
    created tests in tests folder

---
+++ fitness_mcmc/tests/test_model.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class TestModel(TestCase):
+    def test_data_io(self):
+        data = load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
+        assert data['data'][0] == 1682
+
