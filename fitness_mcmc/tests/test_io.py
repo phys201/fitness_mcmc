63bce5ea0470e24ffdeca337bfea2635a2e270e5
Lidiya Ahmed <lidiyayosefah78@gmail.com>
2022-04-19 20:23:04 -0400

    updated test_io
    updated test_io

--- fitness_mcmc/tests/test_io.py
+++ fitness_mcmc/tests/test_io.py
@@ -1,7 +1,16 @@
 from unittest import TestCase
-from fitness_mcmc.data_io import _get_file_path, load_data
+#import fitness_mcmc.data_io as di
+from fitness_mcmc.fitness_mcmc.data_io  import _get_file_path, load_data
 
 class TestIo(TestCase):
     def test_data_io(self):
-        data = load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
+        file_path = di.get_data_file_path('filtered_counts_ypd_temp_30.txt')
+        data = di.load_data(file_path)
+        #data = di.load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
         assert data['data'][0] == 1682
+    def test_simulated_data(self):
+        simulated_file_path = di.get_data_file_path('simulated_filename.txt')
+        simulated_data = di.load_data(simulated_file_path)
+        #data = di.load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
+        assert simulated_data['data'][0] == 1682
+        
--- fitness_mcmc/tests/test_model.py
+++ fitness_mcmc/tests/test_model.py
@@ -1,8 +1,7 @@
 from unittest import TestCase
 
-
 class TestModel(TestCase):
-    def test_data_io(self):
+    def test_sample_lineages(self):
         data = load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
         assert data['data'][0] == 1682
 
