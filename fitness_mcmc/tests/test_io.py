2b715fb2197ee28516907dcb9c722229e009bc1f
Lidiya Ahmed <lidiyayosefah78@gmail.com>
2022-04-19 21:14:55 -0400

    edited tests

--- fitness_mcmc/tests/test_io.py
+++ fitness_mcmc/tests/test_io.py
@@ -1,16 +1,15 @@
 from unittest import TestCase
+#import fitness_mcmc
 #import fitness_mcmc.data_io as di
-from fitness_mcmc.fitness_mcmc.data_io  import _get_file_path, load_data
+from fitness_mcmc.data_io import _get_file_path, load_data
 
 class TestIo(TestCase):
     def test_data_io(self):
-        file_path = di.get_data_file_path('filtered_counts_ypd_temp_30.txt')
-        data = di.load_data(file_path)
+        file_path = _get_file_path('filtered_counts_ypd_temp_30.txt')
+        data = load_data(file_path)
         #data = di.load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
         assert data['data'][0] == 1682
     def test_simulated_data(self):
-        simulated_file_path = di.get_data_file_path('simulated_filename.txt')
-        simulated_data = di.load_data(simulated_file_path)
-        #data = di.load_data('Final_project/fitness_mcmc/filtered_counts_ypd_temp_30.txt')
-        assert simulated_data['data'][0] == 1682
-        
+        simulated_file_path = _get_file_path('simulated_data.txt')
+        simulated_data = load_data(simulated_file_path)
+        assert simulated_data['simulated_data'][0] == 90
