024737630becbd75c5022b2d82471b90b6461d15
Eliot F. Fenton <eliotfenton@gmail.com>
2022-04-19 20:17:43 -0400

    Added function to write simulated data

--- fitness_mcmc/data_io.py
+++ fitness_mcmc/data_io.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 from pathlib import Path
+from fitness_mcmc import create_trajectories, sample_lineages
 
 def _get_file_path(filename):
-    
+
     # Path.cwd() returns the location of the source file currently in use (so
     # in this case io.py). We can use it as base path to construct
     # other paths from that should end up correct on other machines or
@@ -15,19 +16,19 @@ def _get_file_path(filename):
 
 def load_data(filename):
     data_file = _get_file_path(filename)
-    """ 
-    Takes data file path and outputs time generations and counts as numpy arrays 
-    
-    Params: 
-        data_file[str]: path to data_file 
-        
-    Returns: 
+    """
+    Takes data file path and outputs time generations and counts as numpy arrays
+
+    Params:
+        data_file[str]: path to data_file
+
+    Returns:
         data [pandas_dataframe]: actual data where time and counts is stored
         time [array_like]: time generations when sampling would occur
-        counts [array_like]: counts of genotypes 
-        frequencies [array_like]: normalized counts of genotypes 
+        counts [array_like]: counts of genotypes
+        frequencies [array_like]: normalized counts of genotypes
     """
-    
+
     data = pd.read_csv(data_file, delimiter = '\t')
     time = [int(i) for i in data.columns[1:]]
     counts = data.loc[:,data.columns[1:]].to_numpy()
@@ -36,5 +37,30 @@ def load_data(filename):
         frequencies[i] = counts[:,i]/np.sum(counts[:,i])
     idx = np.flipud(np.argsort(np.sum(frequencies.T, axis = 1)))
     ordered_frequencies = frequencies.T[idx,:]
-    
-    return data, time, ordered_frequencies 
+
+    return data, time, ordered_frequencies
+
+def write_simulated_datafile(filename, N = 40, times = -1, s_range = 0.1,
+                             depth = 100):
+    """
+    Creates a textfile of simulated trajectories formated like a real datafile
+
+    Params:
+        filename [str]: name of the output file
+        N [int]: population size
+        times [array_like]: times, in generations, to sample lineages.
+        s_range [float]: range of fitness values
+        depth [int_or_float]: Simulated read depth, affects noise
+    """
+    f0_vals = np.random.random(N)
+    s_vals = np.random.random(N) * s_range
+    if times == -1:
+        times = np.array([5, 10, 25, 40, 45])
+    else:
+        times = np.array(times)
+
+    trajectory = create_trajectories(f0_vals, s_vals, times)
+    sampled = pd.DataFrame(sample_lineages(trajectory, depth * N),
+                           columns = times)
+
+    sampled.to_csv(filename, sep="\t", index_label = "BC")
