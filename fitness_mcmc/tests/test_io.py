eb2f9dce094697b92ed24d57d53c6d46ccf653a2
Lidiya Ahmed <lidiyayosefah78@gmail.com>
2022-04-20 09:33:49 -0400

    name change for simulated my value

--- fitness_mcmc/tests/test_io.py
+++ fitness_mcmc/tests/test_io.py
@@ -5,7 +5,6 @@ class TestIo(TestCase):
     def test_data_io(self):
         """
         gets the path of the file and loads real data, and finds first value of column 5 to compare it with the actual value
-
         """
         file_path = _get_file_path('filtered_counts_ypd_temp_30.txt')
         data = load_data(file_path)
@@ -21,14 +20,13 @@ class TestIo(TestCase):
     def test_simulated_data(self):
         """
         gets the path of the file and loads the simulated data, and finds first value of column 5 to compare it with the actual value
-
         """
         simulated_file_path = _get_file_path('simulated_data.txt')
         simulated_data = load_data(simulated_file_path)
         
-        new_dataframe_2=simulated_data[0]
-        my_value_2=new_dataframe_2['5'][0]
+        new_dataframe_simulated=simulated_data[0]
+        my_value_simulated=new_dataframe_2['5'][0]
         
-        message_2 = "First value and second value are not equal !"
+        message_simulated = "First value and second value are not equal !"
        
-        self.assertEqual(my_value_2, 90, message_2)
+        self.assertEqual(my_value_simulated, 90, message_simulated)
