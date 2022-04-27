ed7796e06e5d46f8a22d82e37a03c5937b02074f
Pavel Zhelnin <pzhelnin@g.harvard.edu>
2022-04-26 23:49:44 -0400

    added more tests

--- fitness_mcmc/tests/test_model.py
+++ fitness_mcmc/tests/test_model.py
@@ -1,18 +1,26 @@
 from unittest import TestCase
-from fitness_mcmc.fitness_mcmc import normalize_func, Fitness_Model
+import fitness_mcmc.fitness_mcmc as fm 
+import fitness_mcmc.data_io as di
 from fitness_mcmc.data_io import load_data
 import numpy as np
 import pymc3 as pm 
 
-data, time, ordered_frequencies= load_data('filtered_counts_ypd_temp_30.txt')
+data, time, ordered_frequencies= di.load_data('filtered_counts_ypd_temp_30.txt')
 
 class TestIo(TestCase):
     
-    fitness_model = Fitness_Model(ordered_frequencies,time) 
+    fitness_model = fm.Fitness_Model(ordered_frequencies,time) 
 
     def test_model_is_valid(self):
         message = "something went wrong with model loading" 
         self.assertTrue(isinstance(self.fitness_model.model, pm.Model),message)
+    
+    def test_sample_lineages(self): 
+        message = "sampling went wrong" 
+        
+        sampled_lineages = fm.sample_lineages(ordered_frequencies,len(ordered_frequencies))
+        
+        self.assertEqual(len(sampled_lineages),len(ordered_frequencies), message)
         
     def test_normalize_func(self):
         """
@@ -21,7 +29,7 @@ class TestIo(TestCase):
         """
         
         x=np.array(10)
-        t=normalize_func(x**2)
+        t=fm.normalize_func(x**2)
         
         message = "First value and second value are not equal !"
        
